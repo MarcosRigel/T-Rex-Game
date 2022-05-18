576b71552382d2402ad7c4104ad61bf36c123ee6
Marcos Rigel <marcosrigel99@gmail.com>
2022-05-18 12:17:55 -0300

    segunda parte

---
+++ script.js
@@ -0,0 +1,34 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    jump();
+  }
+}
+
+function jump() {
+  let position = 0;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upInterval);
+
+      // Descendo
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInteval);
+        } 
+        else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    } else {
+      //subinddo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
+
+document.addEventListener('keyup', handleKeyUp);
